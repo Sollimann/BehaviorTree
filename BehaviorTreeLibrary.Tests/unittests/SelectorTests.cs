2b7bdacc1cf5cee920150a95c1d3b84081c8af9e
sollimann <kristoffer.solberg@cognite.com>
2020-08-16 14:46:17 +0200

    if one child returns success, the next one is not initialized

--- BehaviorTreeLibrary.Tests/unittests/SelectorTests.cs
+++ BehaviorTreeLibrary.Tests/unittests/SelectorTests.cs
@@ -16,12 +16,22 @@ namespace BehaviorTreeLibrary.Tests
                 MockSelector selector = new MockSelector(1);
 
                 selector.Tick();
+
+                // selector is running
                 Assert.AreEqual(selector.Status, Status.BhRunning);
+
+                // first child has not run its TerminateCalled
                 Assert.AreEqual(0, selector[0].TerminateCalled);
 
+                // set the return status of selectors first child
                 selector[0].ReturnStatus = status[i];
                 selector.Tick();
+
+                // Check that the selector status is equal to the return status
+                // of the first child
                 Assert.AreEqual(selector.Status, status[i]);
+
+                // check that the first child has terminated
                 Assert.AreEqual(1, selector[0].TerminateCalled);
             }
         }
@@ -33,6 +43,8 @@ namespace BehaviorTreeLibrary.Tests
             MockSelector selector = new MockSelector(2); 
 
             selector.Tick();
+
+            // Selector is running
             Assert.AreEqual(selector.Status, Status.BhRunning);
 
             // assert that our first child has not had its TerminateCalled
@@ -41,9 +53,43 @@ namespace BehaviorTreeLibrary.Tests
             // set the first child to fail
             selector[0].ReturnStatus = Status.BhFailure;
             selector.Tick();
+
+            // selector is running, but has has not yet succeeded
             Assert.AreEqual(selector.Status, Status.BhRunning);
+
+            // first child terminates
             Assert.AreEqual(1, selector[0].TerminateCalled);
+
+            // second child initialized since selector is still running
             Assert.AreEqual(1, selector[1].InitializeCalled);
+        }
+
+        [Test]
+        public void TickSelectorFirstSecondNotInitializedEndsWithSuccess()
+        {
+            MockSelector selector = new MockSelector(2); // selector with two children
+
+            selector.Tick();
+
+            // Selector is running
+            Assert.AreEqual(selector.Status, Status.BhRunning);
+
+            // terminate has not been called
+            Assert.AreEqual(0, selector[0].TerminateCalled);
+
+            // selector child 1 returns success
+            selector[0].ReturnStatus = Status.BhSuccess;
+
+            selector.Tick();
+            
+            // Selector status returns success
+            Assert.AreEqual(selector.Status, Status.BhSuccess);
+
+            // child 1 successfully terminates
+            Assert.AreEqual(1, selector[0].TerminateCalled);
+
+            // second child is never initialized
+            Assert.AreEqual(0, selector[1].InitializeCalled);
         }
     }
 }
