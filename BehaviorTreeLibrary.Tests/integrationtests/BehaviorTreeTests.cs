aaf53ee100139947b2efe531ea893250eeab3cd6
sollimann <kristoffer.solberg@cognite.com>
2020-08-16 21:41:52 +0200

    added folders for integration tests

---
+++ BehaviorTreeLibrary.Tests/integrationtests/BehaviorTreeTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BehaviorTreeLibrary.Tests.integrationtests
+{
+    public class BehaviorTreeTests
+    {
+    }
+}
