663578759a3ad8fd83c35abf378a38f3b7560c73
sollimann <kristoffer.solberg@cognite.com>
2020-08-16 18:55:31 +0200

    added sequence class mock and the first sequence test

---
+++ BehaviorTreeLibrary.Tests/mocks/MockSequence.cs
@@ -0,0 +1,25 @@
+﻿using BehaviorTreeTests;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BehaviorTreeLibrary.Tests.mocks
+{
+    public class MockSequence : Sequence
+    {
+        // class constructor
+        // calls the base() constructor
+        public MockSequence(int size) : base()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                Children.Add(new MockBehavior());
+            }
+        }
+
+        public MockBehavior this[int i]
+        {
+            get { return Children[i] as MockBehavior;  }
+        }
+    }
+}
---
+++ BehaviorTreeLibrary.Tests/unittests/SequenceTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using BehaviorTreeLibrary;
+using BehaviorTreeLibrary.Tests.mocks;
+
+namespace BehaviorTreeLibrary.Tests.unittests
+{
+    [TestFixture]
+    public class SequenceTests
+    {
+
+        // if our child returns successful, our sequence should return successful
+        // if our child returns false, our sequence should return false
+        public void TickOnePassThroughReturnsResult()
+        {
+            Status[] status = { Status.BhSuccess, Status.BhFailure };
+            for (int i = 0; i < 2; i++)
+            {
+                MockSequence sequence = new MockSequence(1); // one child
+
+
+                sequence.Tick();
+                Assert.AreEqual(sequence.Status, Status.BhRunning);
+                Assert.AreEqual(0, sequence[0].TerminateCalled);
+
+                sequence[0].ReturnStatus = status[i];
+                sequence.Tick();
+                Assert.AreEqual(sequence.Status, status[i]);
+                Assert.AreEqual(1, sequence[0].TerminateCalled);
+            }
+        }
+    }
+}
