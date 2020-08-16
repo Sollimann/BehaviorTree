85c6cf753d665b3b93f5c7763fae77bb6cfb58c7
sollimann <kristoffer.solberg@cognite.com>
2020-08-16 18:55:47 +0200

    added the sequence class

---
+++ BehaviorTree/Sequence.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BehaviorTreeLibrary
+{
+    public class Sequence : Composite
+    {
+        private int _sequence;
+        
+        // constructor
+        public Sequence()
+        {
+            Update = () =>
+            {
+                for (; ; )
+                {
+                    Status s = GetChild(_sequence).Tick();
+                    if (s != Status.BhSuccess)
+                    {
+                        if (s == Status.BhFailure)
+                        {
+                            _sequence = 0;
+                        }
+                        return s;
+                    }
+
+                    // if after I have incremented sequence
+                    if (++_sequence == ChildCount)
+                    {
+                        _sequence = 0;
+                        return Status.BhSuccess;
+                    }
+                }
+            };
+
+            /*
+             * If we return BhRunning, we want to always want to go back and re-initialize
+             * and then work from the beginning of the list/sequence again
+             * */
+
+            Initialize = () => { _sequence = 0; };
+        }
+    }
+}
