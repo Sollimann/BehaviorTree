00ed7b66ec482be74af92ef374ff80df2c5ef59f
sollimann <kristoffer.solberg@cognite.com>
2020-08-17 20:04:30 +0200

    added two unit tests for decorators

---
+++ BehaviorTreeLibrary.Tests/unittests/DecoratorTests.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using BehaviorTreeLibrary;
+using BehaviorTreeLibrary.composite;
+using BehaviorTreeTests;
+
+namespace BehaviorTreeLibrary.Tests.unittests
+{
+    [TestFixture]
+    public class DecoratorTests
+    {
+        [Test]
+        public void TickConditionTrueRunsChild()
+        {
+            int health = 10;
+            Decorator decorator = new Decorator();
+            MockBehavior behavior = decorator.Add<MockBehavior>();
+
+            decorator.CanRun = () =>
+            {
+                if (health < 50)
+                {
+                    return true;
+                }
+                return false;
+            };
+
+            decorator.Tick();
+            Assert.AreEqual(Status.BhRunning, behavior.Status);
+        }
+
+        [Test]
+        public void TickConditionFalseReturnsValue()
+        {
+            int health = 60;
+            Decorator decorator = new Decorator();
+            MockBehavior behavior = decorator.Add<MockBehavior>();
+            decorator.ReturnStatus = Status.BhSuccess;
+
+            decorator.CanRun = () =>
+            {
+                if (health < 50)
+                {
+                    return true;
+                }
+                return false;
+            };
+
+            decorator.Tick();
+            Assert.AreEqual(Status.BhInvalid, behavior.Status);
+            Assert.AreEqual(Status.BhSuccess, decorator.Status);
+        }
+    }
+}
