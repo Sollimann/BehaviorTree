80db65e59539f47cde4aa380cdca7c9888fae2d9
sollimann <kristoffer.solberg@cognite.com>
2020-08-17 19:32:15 +0200

    added condition node

---
+++ BehaviorTree/leaf/Condition.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BehaviorTreeLibrary.leaf
+{
+    public class Condition : Behavior
+    {
+        public Func<bool> CanRun { protected get; set; }
+
+        // constructor
+        public Condition()
+        {
+            Update = () =>
+            {
+                if (CanRun != null && CanRun())
+                {
+                    return Status.BhSuccess;
+                }
+                return Status.BhFailure;
+            };
+        }
+    }
+}
