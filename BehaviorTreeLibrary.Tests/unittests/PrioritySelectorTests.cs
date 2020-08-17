92904aa12119653008bcf42d4ca1ff666561d1a3
sollimann <kristoffer.solberg@cognite.com>
2020-08-17 23:26:36 +0200

    added one more unit test for prioritySelector

--- BehaviorTreeLibrary.Tests/unittests/PrioritySelectorTests.cs
+++ BehaviorTreeLibrary.Tests/unittests/PrioritySelectorTests.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework.Internal;
 using BehaviorTreeLibrary.composite;
 using BehaviorTreeTests;
+using BehaviorTreeLibrary.Tests.mocks;
 
 namespace BehaviorTreeLibrary.Tests.unittests
 {
@@ -31,9 +32,42 @@ namespace BehaviorTreeLibrary.Tests.unittests
         }
 
         [Test]
-        public void TickOnSecondTickReturnsPreviousStatus()
+        public void TickOnSecondTickResetSequences()
         {
+            PrioritySelector selector = new PrioritySelector();
+            MockSequence sequence = new MockSequence(2); // sequence with two children
+            MockSequence sequence1 = new MockSequence(2);
 
+            selector.Add(sequence);
+            selector.Add(sequence1);
+
+            selector.Tick();
+            Assert.AreEqual(Status.BhRunning, selector.Status);
+            sequence[0].ReturnStatus = Status.BhSuccess;
+            sequence[1].ReturnStatus = Status.BhFailure;
+
+            selector.Tick();
+            Assert.AreEqual(Status.BhRunning, selector.Status);
+            Assert.AreEqual(Status.BhRunning, sequence1.Status);
+
+            sequence1[0].ReturnStatus = Status.BhSuccess;
+            sequence1[1].ReturnStatus = Status.BhFailure;
+
+            selector.Tick();
+            Assert.AreEqual(Status.BhFailure, selector.Status);
+            Assert.AreEqual(Status.BhFailure, sequence1.Status);
+
+
+            sequence1[0].ReturnStatus = Status.BhSuccess;
+            sequence1[1].ReturnStatus = Status.BhRunning;
+            selector.Tick();
+            Assert.AreEqual(Status.BhRunning, selector.Status);
+            Assert.AreEqual(Status.BhRunning, sequence1.Status);
+
+            sequence[0].ReturnStatus = Status.BhRunning;
+            selector.Tick();
+            Assert.AreEqual(Status.BhRunning, selector.Status);
+            Assert.AreEqual(Status.BhInvalid, sequence1.Status);
         }
     }
 }
