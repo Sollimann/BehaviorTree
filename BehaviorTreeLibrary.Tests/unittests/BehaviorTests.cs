550da4b94bfc594a80f0e2064b54270e4d719229
sollimann <kristoffer.solberg@cognite.com>
2020-08-17 18:54:43 +0200

    added test skeleton for conditions

--- BehaviorTreeLibrary.Tests/unittests/BehaviorTests.cs
+++ BehaviorTreeLibrary.Tests/unittests/BehaviorTests.cs
@@ -2,7 +2,6 @@
 using NUnit.Framework.Internal;
 using BehaviorTreeLibrary;
 
-
 namespace BehaviorTreeTests
 {
     [TestFixture]
---
+++ BehaviorTreeLibrary.Tests/unittests/ConditionTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using BehaviorTreeLibrary;
+
+namespace BehaviorTreeLibrary.Tests.unittests
+{
+    [TestFixture]
+    public class ConditionTests
+    {
+        [Test]
+        public void TickConditionTrueReturnsSuccess()
+        {
+            /*
+            int health = 10;
+            Condition condition = new Condition();
+            condition.canRun(() =>
+            {
+                if (health < 50)
+                {
+                    return true;
+                }
+                return false;
+            });
+            condition.Tick();
+            Assert.AreEqual(Status.BhSuccess, condition.Status);
+            */
+        }
+    }
+}
